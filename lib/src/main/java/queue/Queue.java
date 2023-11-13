2bb6eb97d6cefed5adcdd750241bb05a04f7f904
Oybek Turaev <turaev_o@auca.kg>
2023-11-13 12:00:51 +0600

    Queue: Implement methods peek(), dequeue()

--- lib/src/main/java/queue/Queue.java
+++ lib/src/main/java/queue/Queue.java
@@ -1,7 +1,5 @@
 package queue;
 
-import java.util.NoSuchElementException;
-
 public class Queue<T> {
     private Node front;
     private Node rear;
@@ -37,6 +35,22 @@ public class Queue<T> {
             oldRear.next = rear;
         }
         n++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("The queue is empty");
+        T item = front.item;
+        front = front.next;
+        n--;
+        if (isEmpty()) {
+            rear = null;
+        }
+        return item;
+    }
+
+    public T peek() {
+        if (isEmpty()) throw new NoSuchElementException("The queue is empty");
+        return front.item;
     }
 
 }
